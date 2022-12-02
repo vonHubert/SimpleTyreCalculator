807758637782114a47b3eba43681792b172848d6
Sergey Sokolov <sergey.and.sokolov@gmail.com>
2022-12-02 23:27:32 +0400

    Initial Commit

---
+++ TyreCalculator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TyreCalculator
+//
+//  Created by MacBook Air 13 on 02.12.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
